6a514c5b973a73200835722c1d5231688a9132e6
yava06 <yanakolosova06@gmail.com>
2019-02-24 23:45:42 +0700

    square

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+
+typedef struct {
+	double x;
+	double y;
+	double r;
+} circle;
+
+double S(double r)
+{
+			double s;
+			s = 3.14 * r * r;
+			return s;
+}
+
 int main()
 {
+		circle num1;
+		double s;
+		printf("Введите параметры x, y, r: ");
+		scanf("%lf %lf %lf" ,&num1.x ,&num1.y ,&num1.r);
+		s = S(num1.r);
+		printf("%.3lf\n", s);
+
     return 0;
 }
